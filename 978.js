6f8a5c537669babf80b99106c5ddd130bc63c5c3
rutaev <prutaev@gmail.com>
2023-06-06 17:13:06 +0300

    first commit

---
+++ 978.js
@@ -0,0 +1,20 @@
+function asd(name) {
+    return name === name.toUpperCase()
+    // return !/[a-z]/.test(name)
+    //!/^[^A-ZА-ЯЁ]*$/.test(s);
+}
+
+function qwe() {
+    console.log(asd("C"));
+}
+
+qwe();
+
+/**
+ * принимаю 2 строки. сколько строк равных первой строке можно построить из букв, содержщихся во второй строке. вернуть число.
+ * 
+ * 
+ */
+
+
+
