4a446100245d66008f5ade05e8b47cc50f907949
piyrutaev <prutaev@gmail.com>
2023-06-09 15:45:55 +0300

    solved Third_Angle_of_a_Triangle

--- 978.js
+++ 978.js
@@ -1,9 +1,9 @@
-function asd(str) {
-    return str.slice(1, -1);
+function asd(a, b) {
+    return 180 - (43 + 78);
 }
 
 function qwe() {
-    console.log(asd('eloquent'));
+    console.log(asd(43, 78));
 }
 
 qwe();
