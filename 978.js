4ec94266c21c2c2a6b023dfff8da3acbb6b37adb
piyrutaev <prutaev@gmail.com>
2023-06-09 23:33:50 +0300

    solved Calculate_BMI

--- 978.js
+++ 978.js
@@ -1,9 +1,13 @@
-function asd(text, char) {
-    return char.repeat(text.length) 
+function asd(weight, height) {
+    let result = weight / (height * height);
+    if(result <= 18.5) return 'Underweight';
+    if(result <= 25.0) return 'Normal';
+    if(result <= 30.0) return 'Overweight';
+    if(result > 30) return 'Obese';
 }
 
 function qwe() {
-    console.log(asd("_3ebzgh4","&"));
+    console.log(asd(80, 1.80));
 }
 
 qwe();
