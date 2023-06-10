a54ca67988df0e848a94eb17450e183d9b85668b
piyrutaev <prutaev@gmail.com>
2023-06-10 23:16:17 +0300

    solved Convert_boolean_values

--- 978.js
+++ 978.js
@@ -1,9 +1,9 @@
-function asd(args) {
-    return Math.min.apply(null, args)
+function asd(bool) {
+    return bool ? 'Yes' : 'No'
 }
 
 function qwe() {
-    console.log(asd([78,56,232,12,8]));
+    console.log(asd(false));
 }
 
 qwe();
