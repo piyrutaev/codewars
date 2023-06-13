5d7755fd2471514e8ada5e3a807616db44602bc3
piyrutaev <prutaev@gmail.com>
2023-06-13 18:07:07 +0300

    Vowel_remover

--- 978.js
+++ 978.js
@@ -1,10 +1,13 @@
-function asd(x) {
-    return typeof(x) === 'string' ? 'Error' : (x * 50) + 6;
-  
+function asd(string) {
+    return string.split(' ').map(el => {
+        return el.replace(/[a, e, i, o, u]/gi, '')
+    }).join(' ');
+
+    // return string.replace(/[aeiou]/g, '')
 }
 
 function qwe() {
-    console.log(asd('sd'));
+    console.log(asd('how are you today?')); //how are you today?
 }
 
 qwe();
