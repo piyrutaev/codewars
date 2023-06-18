2559c6ad9108da832fcf29ca5c5f77ffadf4cb53
piyrutaev <prutaev@gmail.com>
2023-06-18 22:23:09 +0300

    solved Sum_The_Strings

--- 978.js
+++ 978.js
@@ -1,12 +1,11 @@
 function asd(a, b) {
-    return Math.abs(a.reduce((acc, el) => acc * el) - b.reduce((acc, el) => acc * el));
+    return typeof('' + (+a + +b))
+    // return String(+a + +b)
 
-    // let res = [a.reduce((acc, el) => acc * el), b.reduce((acc, el) => acc * el)].sort((a, b) => b - a);
-    // return res[0] - res[1];
 }
 
 function qwe() {
-    console.log(asd([3, 2, 5], [1, 4, 4])); 
+    console.log(asd("4","5")); 
 }
 
 qwe();
