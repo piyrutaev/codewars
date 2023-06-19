ca9202087c9e868fe8b00cb6a6c4b17c5f33c18e
piyrutaev <prutaev@gmail.com>
2023-06-19 23:35:21 +0300

    Pipe_Problem test another solution

--- 978.js
+++ 978.js
@@ -1,11 +1,16 @@
-function asd(a, b) {
-    return '' + (+a + +b);
-    // return String(+a + +b);
+function asd(numbers) {
+    // return Array.from({length: numbers.pop() - numbers[0] + 1}, (_, i) => i + numbers[0]);
 
+    let result = [numbers[0]];
+    while(numbers[0] < numbers.at(-1)) {
+        result.push(numbers[0] + 1)
+        numbers[0]++
+    }
+    return result
 }
 
 function qwe() {
-    console.log(asd('4', '5')); 
+    console.log(asd([])); 
 }
 
 qwe();
