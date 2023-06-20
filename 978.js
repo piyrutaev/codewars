eb83c85cb0bca4623c0615954a41ef9424e4e5bf
piyrutaev <prutaev@gmail.com>
2023-06-20 16:01:25 +0300

    solved Pipe_Problem

--- 978.js
+++ 978.js
@@ -1,5 +1,6 @@
 function asd(numbers) {
-    // return Array.from({length: numbers.pop() - numbers[0] + 1}, (_, i) => i + numbers[0]);
+    
+    return Array(numbers.at(-1) - numbers[0] + 1).fill().map((_, i) => numbers[0] + i)
 
     let result = [numbers[0]];
     while(numbers[0] < numbers.at(-1)) {
@@ -7,6 +8,8 @@ function asd(numbers) {
         numbers[0]++
     }
     return result
+
+    // return Array.from({length: numbers.pop() - numbers[0] + 1}, (_, i) => i + numbers[0]);
 }
 
 function qwe() {
