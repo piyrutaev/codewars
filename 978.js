bd171c83bad057ad0e57b2f49a53f34d8a5e0523
piyrutaev <prutaev@gmail.com>
2023-06-21 18:24:58 +0300

    solved Return_day

--- 978.js
+++ 978.js
@@ -1,41 +1,36 @@
-function asd(operation, value1, value2) {
-  let result;
-  // switch(operation) {
-  //   case '+' :
-  //     result = value1 + value2;
-  //     break;
-  //   case '-' :
-  //     result = value1 - value2;
-  //     break;  
-  //   case '*' :
-  //     result = value1 * value2;
-  //     break;
-  //   case '/' :
-  //     result = value1 / value2;
-  //     break;  
-  //   default : return 'Invalid operator'
-  // }  
-  // return result;
-
-  // switch(operation) {
-  //   case '+' : return value1 + value2;
-  //   case '-' : return value1 - value2;
-  //   case '*' : return value1 * value2;
-  //   case '/' : return value1 / value2;
-  //   default : return 'Invalid operator'
-  // }
-
-  let cases = {
-    '+': value1 + value2,
-    '-': value1 - value2,
-    '*': value1 * value2,
-    '/': value1 / value2,
+function asd(num) {
+  return ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][num - 1] || "Wrong, please enter a number between 1 and 7";
+  
+  
+  return [, "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][num] || "Wrong, please enter a number between 1 and 7";
+  
+  
+  let days = {
+    1: 'Sunday',
+    2: 'Monday',
+    3: 'Tuesday',
+    4: 'Wednesday',
+    5: 'Thursday',
+    6: 'Friday',
+    7: 'Saturday',
   }
-  return cases[operation]
+  return days[ num ] || 'Wrong, please enter a number between 1 and 7';
+
+  
+  switch (num) {
+    case 1: return 'Sunday';
+    case 2: return 'Monday';
+    case 3: return 'Tuesday';
+    case 4: return 'Wednesday';
+    case 5: return 'Thursday';
+    case 6: return 'Friday';
+    case 7: return 'Saturday';
+    default: return 'Wrong, please enter a number between 1 and 7';
+  }
 }
 
 function qwe() {
-    console.log(asd('/', 4, 7)); 
+    console.log(asd(7)); 
 }
 
 qwe();
