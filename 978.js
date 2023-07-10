9f37ceee33b8c041e8f473e7bb1aba71d7154273
piyrutaev <prutaev@gmail.com>
2023-07-11 00:09:36 +0300

    solved Area_or_Perimeter

--- 978.js
+++ 978.js
@@ -1,41 +1,9 @@
-function asd(operation, value1, value2) {
-  let result;
-  // switch(operation) {
-  //   case '+' :
-  //     result = value1 + value2;
-  //     break;
-  //   case '-' :
-  //     result = value1 - value2;
-  //     break;  
-  //   case '*' :
-  //     result = value1 * value2;
-  //     break;
-  //   case '/' :
-  //     result = value1 / value2;
-  //     break;  
-  //   default : return 'Invalid operator'
-  // }  
-  // return result;
-
-  // switch(operation) {
-  //   case '+' : return value1 + value2;
-  //   case '-' : return value1 - value2;
-  //   case '*' : return value1 * value2;
-  //   case '/' : return value1 / value2;
-  //   default : return 'Invalid operator'
-  // }
-
-  let cases = {
-    '+': value1 + value2,
-    '-': value1 - value2,
-    '*': value1 * value2,
-    '/': value1 / value2,
-  }
-  return cases[operation]
+function asd(l , w) {
+    return l == w ? l * w : 2 * (l + w);
 }
 
 function qwe() {
-    console.log(asd('/', 4, 7)); 
+    console.log(asd(6, 10)); 
 }
 
 qwe();
