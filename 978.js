854f244c2854840dcd9e4bfc61e4b15355941b59
piyrutaev <prutaev@gmail.com>
2023-07-11 19:10:41 +0300

    solved Multiplication_table_for_number

--- 978.js
+++ 978.js
@@ -1,10 +1,24 @@
-function asd(s) {
+function asd(number) {
+    // version 1
+    // let i = 1;
+    // let a = ''
+    // while(i <= 10) {
+    //     a += `${i} * ${number} = ${i * number}${i < 10 ? '\n' : ''}`
+    //     i++
+    // }
+    // return a
+
+    // version 2
+    let table = '';
+    for(let i = 1; i <= 10; i++) {
+        table += `${i} * ${number} = ${i * number}${i < 10 ? '\n' : ''}`
+    }
+    return table;
     
-    return ~~(s * 27.7777777778);
 }
 
 function qwe() {
-    console.log(asd(1.08)); // 
+    console.log(asd(5)); 
 }
 
 qwe();
