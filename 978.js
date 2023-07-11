f0f9161bb165caa38f5e251cd36cb3ef59a61906
piyrutaev <prutaev@gmail.com>
2023-07-11 14:46:07 +0300

    solved Include_This

--- 978.js
+++ 978.js
@@ -1,27 +1,9 @@
-function asd(n, m) {
-    // version 1
-    // let initN = n;
-    // if(m <= n) return 'INVALID';
-    // else {
-    //     let arr = [n]
-    //     while(n < m-initN) {
-    //         arr.push(n += initN)
-    //     }
-    //     return arr.reduce((acc, el) => acc + el);
-    // }
-
-    // version 2
-    if(n >= m) return 'INVALID';
-
-    let result = 0;
-    for(let i = n; i < m; i += n) {
-        result += i;
-    }
-    return result;
+function asd(arr, item) {
+    return arr.includes(item);
 }
 
 function qwe() {
-    console.log(asd(2, 9)); // 
+    console.log(asd([1,2,4,5], 3)); // 
 }
 
 qwe();
