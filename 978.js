57490a65d6f5c43344a94b70622da155b0a27cf6
piyrutaev <prutaev@gmail.com>
2023-07-11 14:08:21 +0300

    solved Sum_of_Multiples

--- 978.js
+++ 978.js
@@ -1,9 +1,27 @@
-function asd(l , w) {
-    return l == w ? l * w : 2 * (l + w);
+function asd(n, m) {
+    // version 1
+    // let initN = n;
+    // if(m <= n) return 'INVALID';
+    // else {
+    //     let arr = [n]
+    //     while(n < m-initN) {
+    //         arr.push(n += initN)
+    //     }
+    //     return arr.reduce((acc, el) => acc + el);
+    // }
+
+    // version 2
+    if(n >= m) return 'INVALID';
+
+    let result = 0;
+    for(let i = n; i < m; i += n) {
+        result += i;
+    }
+    return result;
 }
 
 function qwe() {
-    console.log(asd(6, 10)); 
+    console.log(asd(2, 9)); // 
 }
 
 qwe();
