b63cd88d4e895dc97b5a1bad5853ab8005fa40e3
piyrutaev <prutaev@gmail.com>
2023-07-12 15:41:10 +0300

    solved loop_statement

--- 978.js
+++ 978.js
@@ -1,10 +1,31 @@
-function asd(s) {
-    
-    return ~~(s * 27.7777777778);
+function asd(str, n) {
+  // version 1
+  // let result = '';
+  // let count = 1;
+  // while(count <= n) {
+  //   if (count == 1) {
+  //     result = str;
+  //   }
+  //   if (count % 2 == 0) {
+  //     result = result + '*';
+  //   }
+  //   if (count % 2 != 0) {
+  //     result = '*' + result;
+  //   }
+  //   count++;
+  // }
+  // return result;
+
+  // version 2
+  while(n > 0) {
+    str = n % 2 == 0 ? str + '*' : '*' + str;
+    n--;
+  }
+  return str;
 }
 
 function qwe() {
-    console.log(asd(1.08)); // 
+    console.log(asd("a", 5)); // 
 }
 
 qwe();
