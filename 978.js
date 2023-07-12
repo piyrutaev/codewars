127ea48a2f13e17f58b92fa4d819ca711976f29b
piyrutaev <prutaev@gmail.com>
2023-07-12 17:54:17 +0300

    solved Name_on_billboard

--- 978.js
+++ 978.js
@@ -1,41 +1,29 @@
-function asd(month) {
+function asd(name, price = 30) {
   // version 1
-  // switch(month) {
-  //   case 4: return 30;
-  //   case 6: return 30;
-  //   case 9: return 30;
-  //   case 11: return 30;
-  //   case 2: return 28;
-  //   default: return 31
+  // let result = 0;
+  // let pricePerCharacter = price;
+  // for(let i = 0; i < name.length; i++) {
+  //   result += pricePerCharacter;
   // }
+  // return result
 
   // version 2
-  // switch(month) {
-  //   case 2: return 28;
-  //   case 4: 
-  //   case 6: 
-  //   case 9: 
-  //   case 11: return 30;
-  // }
-  // return 31;
+//   let result = 0;
+//   for (let i = 0; i < name.length; i++) {
+//     result += price;
+//   }
+//   return result;
 
-  // version 3
-  let days;
-  switch(month) {
-    case 2: days = 28;
-    break;
-    case 4: 
-    case 6: 
-    case 9: 
-    case 11: days = 30;
-    break;
-    default: days = 31;
-  }
-  return days;
+
+// version 3
+// return name.split('').reduce(acc => acc + price, 0);
+
+// version 4
+return name.repeat(price).length;
 }
 
 function qwe() {
-    console.log(asd(11)); // 
+    console.log(asd("Jeong-Ho Aristotelis"));
 }
 
 qwe();
