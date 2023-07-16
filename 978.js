46139a187f2c2a80743cd92e42b9a70a74a4a469
piyrutaev <prutaev@gmail.com>
2023-07-16 23:40:00 +0300

    solved Define_a_card_suit

--- 978.js
+++ 978.js
@@ -1,9 +1,16 @@
-function asd(name, city, state) {
-    return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
+function asd(card) {
+    let suits = {
+        9824: 'spades',
+        9827: 'clubs',
+        9829: 'hearts',
+        9830: 'diamonds',
+    }
+    return suits[card.charCodeAt(card.length-1)]
+
 }
 
 function qwe() {
-    console.log(asd(['Franklin','Delano','Roosevelt'], 'Chicago', 'Illinois')); 
+    console.log(asd('Q♠')); 
 }
 
 qwe();
