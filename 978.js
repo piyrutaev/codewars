d8598b0a3753c6a062e3aee8ab885a368c49ce45
piyrutaev <prutaev@gmail.com>
2023-07-19 13:18:57 +0300

    solved String_cleaning

--- 978.js
+++ 978.js
@@ -1,11 +1,11 @@
 function asd(card) {
-    // return card == '' ? '' : 'sex '.repeat(card.split(' ').length).slice(0, -1)
-    return card.replace(/[^ ]+/g, 'sex')
+    // return card.replace(/[0-9]/g, '')
+    return card.replace(/\d/g, '')
 
 }
 
 function qwe() {
-    console.log(asd('This is a sample sentence.')); 
+    console.log(asd('(E3at m2e2!!)')); 
 }
 
 qwe();
