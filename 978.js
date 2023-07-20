3f522492cf3a9bb92062cd3659d8396f62b0be01
piyrutaev <prutaev@gmail.com>
2023-07-20 10:10:30 +0300

    solved Trip_to_the_Market

--- 978.js
+++ 978.js
@@ -1,10 +1,11 @@
-function asd(string) {
-    return `${string.replace(/[!]/g, '')}!`
+function asd(s) {
+    return /tree fiddy|3.50|three fifty/.test(s);
+    return s.includes('tree fiddy') || s.includes('3.50') || s.includes('three fifty');
 
 }
 
 function qwe() {
-    console.log(asd("Hi")); 
+    console.log(asd("Your girlscout cookies are ready to ship. Your total comes to tree fiddy")); 
 }
 
 qwe();
