c9ff765baf62e88e0aacedcd27be47b20ea6d7df
piyrutaev <prutaev@gmail.com>
2023-07-21 16:15:03 +0300

    solved functions_odd

--- 978.js
+++ 978.js
@@ -1,30 +1,9 @@
-function asd(param) {
-    // let a = {
-    // "jabroni":	"Patron Tequila",
-    // "school Counselor":	"Anything with Alcohol",
-    // "programmer":	"Hipster Craft Beer",
-    // "bike Gang Member":	"Moonshine",
-    // "politician":	"Your tax dollars",
-    // "rapper":	"Cristal",
-    // "else":	"Beer"
-    // }
-
-    // return a[param.toLowerCase()] ? a[param.toLowerCase()] : a['else'];
-
-    let a = {
-        "jabroni":	"Patron Tequila",
-        "school counselor":	"Anything with Alcohol",
-        "programmer":	"Hipster Craft Beer",
-        "bike gang member":	"Moonshine",
-        "politician":	"Your tax dollars",
-        "rapper":	"Cristal",
-        }
-
-        return a[param.toLowerCase()] || 'Beer'
+function asd(values) {
+    return values.filter(el => el % 2 != 0);
 }
 
 function qwe() {
-    console.log(asd("School Counselo")); 
+    console.log(asd([2, 4, 6])); 
 }
 
 qwe();
