855a625eb48cac940ce34a97617a409dbf5b7ca1
piyrutaev <prutaev@gmail.com>
2023-07-21 15:58:58 +0300

    solved drinks

--- 978.js
+++ 978.js
@@ -1,9 +1,30 @@
-function asd(obj, feature) {
-    return `${obj} are ${feature}`
+function asd(param) {
+    // let a = {
+    // "jabroni":	"Patron Tequila",
+    // "school Counselor":	"Anything with Alcohol",
+    // "programmer":	"Hipster Craft Beer",
+    // "bike Gang Member":	"Moonshine",
+    // "politician":	"Your tax dollars",
+    // "rapper":	"Cristal",
+    // "else":	"Beer"
+    // }
+
+    // return a[param.toLowerCase()] ? a[param.toLowerCase()] : a['else'];
+
+    let a = {
+        "jabroni":	"Patron Tequila",
+        "school counselor":	"Anything with Alcohol",
+        "programmer":	"Hipster Craft Beer",
+        "bike gang member":	"Moonshine",
+        "politician":	"Your tax dollars",
+        "rapper":	"Cristal",
+        }
+
+        return a[param.toLowerCase()] || 'Beer'
 }
 
 function qwe() {
-    console.log(asd("Animals","Good")); 
+    console.log(asd("School Counselo")); 
 }
 
 qwe();
