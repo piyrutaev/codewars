12b345688d0f046207fa6b73ef3f1339652c75b2
piyrutaev <prutaev@gmail.com>
2023-07-23 00:20:44 +0300

    solved Polish_alphabet

--- 978.js
+++ 978.js
@@ -1,9 +1,21 @@
-function asd(obj, feature) {
-    return `${obj} are ${feature}`
+function asd(string) {
+    let a = {
+    'ą': 'a',
+    'ć': 'c',
+    'ę': 'e',
+    'ł': 'l',
+    'ń': 'n',
+    'ó': 'o',
+    'ś': 's',
+    'ź': 'z',
+    'ż': 'z'
+    }
+
+    return string.replace(/[ąćęłńóśźż]/g, el => a[el])
 }
 
 function qwe() {
-    console.log(asd("Animals","Good")); 
+    console.log(asd("Jędrzej Błądziński")); 
 }
 
 qwe();
