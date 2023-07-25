7507a5c83dcf382f7ba57e78e780c9ed7f93aef7
piyrutaev <prutaev@gmail.com>
2023-07-26 00:10:31 +0300

    solved tips

--- 978.js
+++ 978.js
@@ -1,9 +1,21 @@
-function asd(c) {
-    return c.charCodeAt();
-}
+function asd(amount, rating) {
+    const TIPS = {
+  "terrible": 0.0,
+  "poor": 0.05,
+  "good": 0.1,
+  "great": 0.15,
+  "excellent": 0.2
+};
+
+
+  rating = rating.toLowerCase();
+  
+  return rating in TIPS ? Math.ceil(TIPS[rating] * amount) : "Rating not recognised";
+};
+
 
 function qwe() {
-    console.log(asd('A')); 
+    console.log(asd(20, 'excellent')); 
 }
 
 qwe();
