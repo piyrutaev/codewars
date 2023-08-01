728cb6a71155430420feb85481a4f32e1e0acda8
piyrutaev <prutaev@gmail.com>
2023-08-02 00:27:54 +0300

    solved 8_Whose_Move

--- 978.js
+++ 978.js
@@ -1,34 +1,15 @@
-function asd(n) {
-    // return n > 0 ? [...Array(n+1)].map((_, index) => index--).reduce((acc, el) => acc + el) : 0;
-
-
-    // if(n > 0) {
-    //     let a = n;
-    //     for(n; n > 1; n--) {
-    //     a += n - 1;
-    // }
-    // return a;
-    // }
-    // return 0;
-
-
-    // let a = 0;
-    // while(n > 0) {
-    //     a += n;
-    //     n--;
-    // }
-    // return a;
-
-
-    return n > 0 ? n * (n + 1) / 2 : 0;
-
-    
+function asd(lastPlayer, win) {
+      let oppositePlayer = {
+        'white': 'black',
+        'black': 'white',
+      }
+      return win ? lastPlayer : oppositePlayer[lastPlayer];
 };
 
 
 
 function qwe() {
-    console.log(asd(-9)); 
+    console.log(asd("black", true)); 
 }
 
 qwe();
