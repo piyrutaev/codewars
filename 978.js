0ead9ba6c565cb2cea45512b74ab3a48b7df8d9a
piyrutaev <prutaev@gmail.com>
2023-08-03 22:58:30 +0300

    solved 7_Knight_vs_King

--- 978.js
+++ 978.js
@@ -1,15 +1,16 @@
-function asd(lastPlayer, win) {
-      let oppositePlayer = {
-        'white': 'black',
-        'black': 'white',
-      }
-      return win ? lastPlayer : oppositePlayer[lastPlayer];
+function asd(knightPosition, kingPosition) {
+  let files = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+  let filesPosition = Math.abs(files.indexOf(knightPosition[1]) - files.indexOf(kingPosition[1]));
+  let rankPosition = Math.abs(knightPosition[0] - kingPosition[0]);
+  if(filesPosition == 1 && rankPosition == 2 || filesPosition == 2 && rankPosition == 1) return 'Knight';
+  if(filesPosition < 2 && rankPosition <= 1) return 'King';
+  return 'None';
 };
 
 
 
 function qwe() {
-    console.log(asd("black", true)); 
+    console.log(asd([1,"B"], [1,"C"])); 
 }
 
 qwe();
