8603e0639834fae2426b2af4524990b4142ff324
piyrutaev <prutaev@gmail.com>
2023-08-05 23:57:12 +0300

    solved 8_Get_number_from_string

--- 978.js
+++ 978.js
@@ -1,22 +1,11 @@
-function asd(text, n, oldValue, newValue) {
-  // let regex = new RegExp("((?:[^" +oldValue+ "]*" +oldValue+ "){" + (n-1) + "}[^" +oldValue+ "]*)" +oldValue, "g");
-  // return text.replace(regex, `$1${newValue}`);
-
-  if(n > 0) {
-    let count = 0;
-    let regexp = new RegExp(oldValue, 'g');
-    return text.replace(regexp, match => {
-      count++;
-      return (count % n === 0) ? newValue : match;
-    })
-  }
-  return text;
+function asd(s) {
+  return s.replace(/\D/g, '');
 };
 
 
 
 function qwe() {
-    console.log(asd("Vader said: No, I am your father!", 1, 'a', 'o')); 
+    console.log(asd("this is number: 7")); 
 }
 
 qwe();
