c129223b0e0550d51954e972fd1f37a1702d1d68
piyrutaev <prutaev@gmail.com>
2023-08-08 23:00:26 +0300

    solved 7_The_mean_of_two_means

--- 978.js
+++ 978.js
@@ -1,11 +1,16 @@
-function asd(items, a, b) {
-    // let one = items.slice(0, a)
-    // let two = items.slice(b)
-    return items.slice(0, a).concat(items.slice(b))
+function asd(arr, x, y) {
+    if(x > 1 && y > 1 && x <= arr.length && y <= arr.length) {
+        let one = arr.slice(0, x).reduce((acc, el) => acc + el) / x;
+        let two = arr.slice(-y).reduce((acc, el) => acc + el) / y;
+        return (one + two) / 2;
+    }
+    return -1;
+    
+    
 }
 
 function qwe() {
-    console.log(asd([12, 14, 63, 72, 55, 24], 2, 4)); 
+    console.log(asd([1,3,2,4],2,8)); 
 }
 
 qwe();
