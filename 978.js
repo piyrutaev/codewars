4afc00cc6775dde65676070ccda2fba6bb151e95
piyrutaev <prutaev@gmail.com>
2023-08-15 22:58:23 +0300

    solved 8_trim_and_the_string_template

--- 978.js
+++ 978.js
@@ -1,12 +1,16 @@
-function asd(message) {
-    // let arr = message.map(el => parseInt(el, 2));
-    // return arr.map(el => String.fromCharCode(el)).join('');
+function asd(s) {
+    let str = s.trim();
+    let res = '';
 
-    return message.map(el => parseInt(el, 2)).map(el => String.fromCharCode(el)).join('');
+    for(let i = 0; i <= 5; i++) {
+        res = res + str.repeat(i) + '\n';
+    }
+    return res.trim();
 }
 
 function qwe() {
-    console.log(asd(['01000001', '01101100', '01100101', '01111000', '01100001', '01101110', '01100100', '01100101', '01110010'])); // ['01000001', '01101100', '01100101', '01111000', '01100001', '01101110', '01100100', '01100101', '01110010']
+    console.log(asd("  a")); 
 }
+            
 
 qwe();
