c98fb96d3f11adfbbd7ba2c76f2128fc070d212f
piyrutaev <prutaev@gmail.com>
2023-10-05 23:13:17 +0300

    solved Array_plus_array

--- 978.js
+++ 978.js
@@ -1,14 +1,45 @@
 function asd(arr) {
-    let a = arr.reduce((acc, el) => el.length < acc ? el.length : acc, Infinity);
-    return arr.map(el => el.substring(0, a));
+    // переводит элементы массива в свойства объекта
+    let obj = arr.reduce((object, value) => {
+        return {...object, [value]: 0}
+    }, {})
+    
+    // присваивает свойствам объекта значения, которые равны сумме вхождений элемента в массив (сколько раз элемент встречается в массиве)
+    arr.map(el => {
+        return obj[el] += 1
+    })
 
+    // создает массив из значений свойств объекта
+    let sd = Object.values(obj);
 
-    const minLength = Math.min(...arr.map(x => x.length));
-     return arr.map(x => x.slice(0, minLength));
+    // сортирует массив от меньшего к большему
+    let sorted = sd.sort((a, b) => a - b);
+    
+    let result = [];
+    
+    // Object.keys(obj) создает массив из свойств объекта.
+    // find последовательно берет каждый элемент из массива с ключами, находит его значение в объекте obj
+    // и сранивает его со значением из отсортированного массива sorted, если значения совпадают, ключ добавляется в массив result     
+    for(let i = 0; i < sorted.length; i++) {
+        result.push(Object.keys(obj).find(key => obj[key] === sorted[i]))
+    }
+
+    let sdsd = []
+
+    // .push([...Array(sorted[i])] - добавляет в массив sdsd новый массив, длинной, равной значению элемента из массива sorted
+    // .fill(result[i]) - заполняет всю длину добавленного массива одним значением, взятым из массива result
+    for(let i = 0; i < sorted.length; i++) {
+        sdsd.push([...Array(sorted[i])].fill(result[i]))
+    }
+    return [].concat(...sdsd).map(el => +el);
+
+    
+    
 }
 
 function qwe() {
-    console.log(asd(["wert", "abc","deerv","ijklmn"])); 
+    console.log(asd([1,1,1,2,2,3])); 
+    // console.log(asd(['aew', 'gf', 'as', 'gf', 'gf', 'aew'])); 
 }
             
 
---
+++ test.js
@@ -0,0 +1,11 @@
+function asd(a, b) {
+    return a.concat(b).reduce((acc, el) => acc + el);
+}    
+
+
+function qwe() {
+    console.log(asd([2, 4, 8], [2, 4, 6])); 
+}
+            
+
+qwe();
