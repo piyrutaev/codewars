40ce9b69b8a6423e819dbedf3a59ca6cdf3deaee
piyrutaev <prutaev@gmail.com>
2024-08-26 16:04:19 +0300

    Решение задачи

---
+++ countOccurrences.js
@@ -0,0 +1,8 @@
+function countOccurrences(arr) {
+    return arr.reduce((acc, item) => {
+        acc[item] = (acc[item] || 0) + 1;
+        return acc;
+    }, {});
+}
+
+console.log(countOccurrences(['a', 'b', 'c', 'b', 'a', 'b']));
